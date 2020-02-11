7b09ee5fec129d8de0d0418d8eed1c7cf05990f8
BrunoJunior <bruno.desprez@gmail.com>
2020-02-11 12:15:29 +0100

    Fixes #19 - On limite la taille du fichier à 1Mo max

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -17,6 +17,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class TicketController extends MyController
 {
@@ -44,6 +47,9 @@ class TicketController extends MyController
      * @return RedirectResponse|Response
      * @throws DBALException
      * @throws UnknownEumException
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
     public function nouveauTicket(Request $request, Kermesse $kermesse)
     {
