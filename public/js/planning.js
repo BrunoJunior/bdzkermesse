b727081ea5c07b6e19775150aee57e71d2ecd3dd
Thalassa Ingénierie Web <thalassa.dev@gmail.com>
2020-02-17 23:01:56 +0100

    Inversion afficher/masquer créneaux incomplets

--- public/js/planning.js
+++ public/js/planning.js
@@ -1,7 +1,7 @@
 $(function() {
     const hauteurEnteteInit = $(".planning-entete").height();
     const fnDisplayCreneaux = function () {
-        $('.activite-creneau.creneau-incomplet').toggle($("#afficherIncomplets").prop('checked'));
+        $('.activite-creneau.creneau-incomplet').toggle(!$("#afficherIncomplets").prop('checked'));
         $(".planning-entete").height((100 * $("#planning").height() / hauteurEnteteInit) + "%");
     };
     $("#afficherIncomplets").on('change', fnDisplayCreneaux);
