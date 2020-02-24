caa86dc3208b6d01aab679c96897dde155242a9c
BrunoJunior <bruno.desprez@gmail.com>
2020-02-24 10:54:16 +0100

    [FIX] Script de déploiement

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 git pull
 sudo -u www-data composer install --no-dev --optimize-autoloader --classmap-authoritative
-sudo -u www-data APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
-sudo -u www-data php bin/console doctrine:migrations:migrate
+sudo -u www-data APP_ENV=prod APP_DEBUG=0 php bin/console --no-interaction cache:clear
+sudo -u www-data php bin/console --no-interaction doctrine:migrations:migrate
 chown www-data:www-data -R ./*
-server apache2 restart
+service apache2 restart
