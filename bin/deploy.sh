c5ae33fd5e427b757fd537f186db8c8e4792fe9f
BrunoJunior <bruno.desprez@gmail.com>
2020-02-28 16:32:51 +0100

    Webpack production build

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 git pull
+sudo -u www-data yarn encore production
 sudo -u www-data composer install --no-dev --optimize-autoloader --classmap-authoritative
 sudo -u www-data APP_ENV=prod APP_DEBUG=0 php bin/console --no-interaction cache:clear
 sudo -u www-data php bin/console --no-interaction doctrine:migrations:migrate
